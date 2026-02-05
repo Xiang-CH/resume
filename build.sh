bdd0c23bdfc3330247eff433756acd2b68646333
Xiang <cxiang@connect.hku.hk>
2026-02-05 14:36:05 +0800

    content update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env
-xelatex -jobname resume_chen_xiang_with_phone "\def\phonenumber{${PHONE_NUMBER}}\input{main.tex}"
+xelatex -jobname resume_chen_xiang_with_phone "\def\phonenumber{${PHONE_NUMBER_ONE}}\def\phonenumbertwo{${PHONE_NUMBER_TWO}}\input{main.tex}"
 xelatex -jobname resume_chen_xiang "\def\showphone{false}\input{main.tex}"
 rm *.aux *.log *.out
